80a1bb26a6ed41809e2533cc4e6921db30059e23
Linc <test@testserver.com>
2018-04-03 15:29:47 -0500

    panic upload to a branch other than master

--- lmnop_project/settings.py
+++ lmnop_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8c01$#j44g3znb)$q0()8)!%ts-jc)k12!a75-!63qb%bj=d4k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'lmnop_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'lmnop',
-        'USER' : 'lmnop',
-        'PASSWORD' : os.environ['LMNOP_DB_PW'],
-        'HOST' : 'localhost',
-        'PORT' : '5432',
+        # 'USER': 'lmnop',
+        # 'PASSWORD': os.environ['LMNOP_DB_PW'],
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
