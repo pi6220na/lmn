e07f84e60142775180521ce310caca7e9416d45c
JL <lollypopsandrainbows@gmail.com>
2018-04-04 11:33:13 -0500

    Added comments to forms.py

--- lmn/forms.py
+++ lmn/forms.py
@@ -6,15 +6,15 @@ from django.contrib.auth.models import User
 from django.forms import ValidationError
 
 
-class VenueSearchForm(forms.Form):
+class VenueSearchForm(forms.Form): #creates class form with input restriction 
     search_name = forms.CharField(label='Venue Name', max_length=200)
 
 
-class ArtistSearchForm(forms.Form):
+class ArtistSearchForm(forms.Form): #creates class form with input restriction 
     search_name = forms.CharField(label='Artist Name', max_length=200)
 
 
-class NewNoteForm(forms.ModelForm):
+class NewNoteForm(forms.ModelForm): #creates class form with input restriction 
     class Meta:
         model = Note
         fields = ('title', 'text')
@@ -22,25 +22,25 @@ class NewNoteForm(forms.ModelForm):
 
 class UserRegistrationForm(UserCreationForm):
 
-    class Meta:
-        model = User
+    class Meta:#creates class form with input restriction 
+        model = User  #this is the user. This is used to validate information
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 
 
-    def clean_username(self):
+    def clean_username(self): #used to create username
 
         username = self.cleaned_data['username']
 
         if not username:
-            raise ValidationError('Please enter a username')
+            raise ValidationError('Please enter a username') #raises error if username is not valid
 
         if User.objects.filter(username__iexact=username).exists():
-            raise ValidationError('A user with that username already exists')
+            raise ValidationError('A user with that username already exists') #if this username exists already error is raised
 
         return username
 
 
-    def clean_first_name(self):
+    def clean_first_name(self): #validates first name
         first_name = self.cleaned_data['first_name']
         if not first_name:
             raise ValidationError('Please enter your first name')
@@ -48,7 +48,7 @@ class UserRegistrationForm(UserCreationForm):
         return first_name
 
 
-    def clean_last_name(self):
+    def clean_last_name(self): #validates last name
         last_name = self.cleaned_data['last_name']
         if not last_name:
             raise ValidationError('Please enter your last name')
@@ -56,12 +56,12 @@ class UserRegistrationForm(UserCreationForm):
         return last_name
 
 
-    def clean_email(self):
+    def clean_email(self): #validates email
         email = self.cleaned_data['email']
         if not email:
             raise ValidationError('Please enter an email address')
 
-        if User.objects.filter(email__iexact=email).exists():
+        if User.objects.filter(email__iexact=email).exists(): #checks to make sure there are no duplicates
             raise ValidationError('A user with that email address already exists')
 
         return email
@@ -69,12 +69,12 @@ class UserRegistrationForm(UserCreationForm):
 
     def save(self, commit=True):
         user = super(UserRegistrationForm, self).save(commit=False)
-        user.username = self.cleaned_data['username']
+        user.username = self.cleaned_data['username'] #saves clean data
         user.email = self.cleaned_data['email']
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
 
         if commit:
-            user.save()
+            user.save() #saves user info
 
         return user
