de81002a6b0f0b7f90adcd6f8c02fd1ce09ffb7d
pamsfields <jh5811zj@go.minneapolis.edu>
2018-04-14 23:18:30 -0500

    added loguout to views

--- lmn/views.py
+++ lmn/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def homepage(request):
     return render(request, 'lmn/home.html')
 
+def logged_out(request):
+    return render(request, 'lmn/logged_out.html')
