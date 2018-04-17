13ed18a083a2d441d71addd318284f5493ddfbd3
pamsfields <jh5811zj@go.minneapolis.edu>
2018-04-16 22:37:06 -0500

    updated userinfo and user profile, but still getting 500 error when attempting to visit on site

--- lmn/views_users.py
+++ lmn/views_users.py
@@ -11,12 +11,17 @@ from django.core.exceptions import PermissionDenied
 from django.utils import timezone
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.http import HttpResponse, Http404
+from PIL import Image
+import io
 
 
 def user_profile(request, user_pk):
     user = User.objects.get(pk=user_pk)
     usernotes = Note.objects.filter(user=user.pk).order_by('posted_date').reverse()
-    bio_info = user.userinfo.user_bio_info if user.userinfo else "YO"
+    if hasattr(user, 'userinfo') and hasattr(user.userinfo, 'user_bio_info'):
+        user_bio_info = user.userinfo.user_bio_info
+    else:
+        user_bio_info = 'Still a work in progress.'
     return render(request, 'lmn/users/user_profile.html', {'user' : user , 'notes' : usernotes , 'bio_info' : bio_info})
 
 def user_profile_photo(request, user_pk):
@@ -34,32 +39,35 @@ def edit_user_profile(request):
     if request.method == 'POST':
         form = UserEditForm(request.POST, request.FILES)
         if form.is_valid():
-            user.username = form.cleaned_data["user_name"]
-            user.first_name = form.cleaned_data["user_first"]
-            user.last_name = form.cleaned_data["user_last"]
-            user.email = form.cleaned_data["user_email"]
-            user_bio_info=form.cleaned_data["user_bio_info"]
-            user_photo = request.FILES["user_photo"]
-
-            if user.userinfo is None:
-                user.userinfo = UserInfo()
+            user.username = form.cleaned_data["user_name", False]
+            user.first_name = form.cleaned_data["user_first", False]
+            user.last_name = form.cleaned_data["user_last", False]
+            user.email = form.cleaned_data["user_email", False]
+            user_bio_info=form.cleaned_data["user_bio_info", False]
+            user_photo = request.FILES["user_photo", False]
 
             user.userinfo.user_bio_info = user_bio_info
-            #user.userinfo.user_photo_name = photo.name
-            #user.userinfo.user_photo = photo.read()
+            if hasattr(photo) and user_photo_file_name is not None:
+                user.userinfo.user_photo_name = photo.name
+                user.userinfo.user_photo = photo
 
             user.save()
             user.userinfo.save()
-        else:
-            raise RuntimeError(form.errors)
 
-    uinfo = user.userinfo
-    if uinfo:
-        user_bio_info = uinfo.user_bio_info
-        #photo = uinfo.user_photo
+        if hasattr(user, 'userinfo') and user.userinfo is not None:
+            user_bio_info = user.userinfo.user_bio_info
+            photo = user.userinfo.user_photo
+        else:
+            user.userinfo = UserInfo()
+            about_me = "Dancing to the music in my head"
+            user.save()
+            user.userinfo.save()
+
+        user.save()
+        user.userinfo.save()
+
     else:
-        user_bio_info = "I dance to the music in my head!"
-        photo = None
+        raise RuntimeError(form.errors)
 
     form = UserEditForm({"user_name": user.username,
                          "user_first": user.first_name,
@@ -68,7 +76,7 @@ def edit_user_profile(request):
                          "user_bio_info": user.user_bio_info,
                          "user_photo": user.photo})
 
-    return redirect('lmn:user_profile', user_pk=request.user.pk)
+    return render(request, 'lmn/users/edit_user_profile.html', {'form': form, 'user': user})
 
 
 
@@ -81,6 +89,10 @@ def register(request):
             user = form.save()
             user = authenticate(username=request.POST['username'], password=request.POST['password1'])
             login(request, user)
+            user_info = UserInfo()
+            user_info.user = user
+            user_info.save()
+
             return redirect('lmn:homepage')
 
         else :
@@ -95,4 +107,4 @@ def register(request):
 def logout_view(request):
     response = logout(request)
     message = 'You have been logged out\n Goodbye!'
-    return render(request, 'registration/logout.html', {'message':message})
+    return render(request, 'registration/logout.html', {'message': message })
