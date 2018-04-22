eaacf994f6cad9d3e5c3068cffa964f23174bc6a
pi6220na <you@example.com>
2018-04-22 14:27:55 -0500

    Add delete button for Artist and Venue detail pages

--- lmn/urls.py
+++ lmn/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     url(r'^venues/list/$', views_venues.venue_list, name='venue_list'),
     url(r'^venues/detail/(?P<venue_pk>\d+)/$', views_venues.venue_detail, name='venue_detail'),
     url(r'^venues/artists_at/(?P<venue_pk>\d+)/$', views_venues.artists_at_venue, name='artists_at_venue'),
+    url(r'^venues/delete/$', views_venues.delete_venue, name='delete_venue'),
 
     # Note related
     url(r'^notes/latest/$', views_notes.latest_notes, name='latest_notes'),
@@ -25,6 +26,7 @@ urlpatterns = [
     url(r'^artists/list/$', views_artists.artist_list, name='artist_list'),
     url(r'^artists/detail/(?P<artist_pk>\d+)/$', views_artists.artist_detail, name='artist_detail'),
     url(r'^artists/venues_played/(?P<artist_pk>\d+)/$', views_artists.venues_for_artist, name='venues_for_artist'),
+    url(r'^artists/delete/$', views_artists.delete_artist, name='delete_artist'),
 
     # User related
     url(r'^user/profile/(?P<user_pk>\d+)/$', views_users.user_profile, name='user_profile'),
